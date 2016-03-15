1fac6ba4c2f85991992e14680542f64cce9e9217
Daniel de la Cruz & lou <pair+danieldelacruz01.louise-r-blue@devbootcamp.com>
2016-03-15 16:40:02 +1300

    Add hyperscript, -html form and setInterval

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("welcome to chat-client")
+//console.log("welcome to chat-client")
+var h = require('hyperscript');
+var $ = require("jquery");
+
+
+window.setInterval(refreshMsg, 5000)
+function refreshMsg(){
+  $.get("http://localhost:3000/v1/messages")
+  .done(function(data){
+    for (var i = 0; i < data.messages.length; i++){
+      var sender = data.messages[i].sender
+      var message = data.messages[i].message
+      var html = h('div.message', {style: {'font-size': '1.5em'}}, h('p', {}, sender + ": " + message), h('hr',{}))
+      $('main').append(html)
+    }
+  })
+  .fail(function(err){
+    console.log(err);
+  })
+}
+$(document).ready(function(){
+  $('form').submit(function(event){
+    var newMessage = {}
+    newMessage["sender"] = $('input#name').val()
+    newMessage["message"] = $('input#message').val()
+    $.post("http://localhost:3000/v1/messages", newMessage)// function(){
+
+    // })
+    // .done(function() {
+    //   console.log('new message sent!')
+    // })
+    // .fail(function() {
+    //   console.log( 'error' );
+    // })
+})
+})
